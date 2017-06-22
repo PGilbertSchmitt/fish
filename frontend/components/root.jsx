4d3798ce8bcd20b68336a1a3850d7bd4d22bb779
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-22 12:47:45 -0700

    Add basic react entry point

---
+++ frontend/components/root.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Root = (props) => (
+    <h1>Hello, my dudes!</h1>
+);
+
+export default Root;
--- frontend/fish.jsx
+++ frontend/fish.jsx
@@ -1,1 +1,8 @@
-// Nothing, but not for tryin'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './components/root';
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(<Root />, document.getElementById("root"));
+})
