bfcd9f0ff44c428adffdd4dc312f441230848f61
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-26 12:20:13 -0700

    Add bottom spacer component

--- frontend/components/core/app.jsx
+++ frontend/components/core/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Main from './main';
-import Header from '../nav/header'
+import Header from '../nav/header';
+import Spacer from '../extra/spacer';
 
 const App = ({ children }) => (
     <div>
         <Header />
         <Main />
+        <Spacer />
     </div>
 )
 
---
+++ frontend/components/extra/spacer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Spacer = props => (
+    <div className="spacer"></div>
+);
+
+export default Spacer;
