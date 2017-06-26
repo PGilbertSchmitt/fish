849bbcc9ef92b59ad234ae6f5bdd4637313b3c34
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-26 13:25:35 -0700

    Add better aesthetics

--- frontend/components/pages/home.jsx
+++ frontend/components/pages/home.jsx
@@ -5,6 +5,17 @@ const Home = (props) => (
     <section className="section">
         <div className="self">
             <img className="self-image" src="images/body_circle.gif" />
+
+            <div className="about">
+                <p>
+                    23 years young software developer. I like to make computers beep for fun.
+                </p>
+                <p>
+                    PHONE: 561-319-2333
+                    <br />
+                    EMAIL: PGilbertSchmitt@gmail.com
+                </p>
+            </div>
         </div>
 
         <div className="portal">
