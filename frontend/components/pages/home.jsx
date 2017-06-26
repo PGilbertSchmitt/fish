2fb6ca8614c2fe3361f02972787bc4875d5b828e
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-25 17:51:31 -0700

    Begin styling homepage

--- frontend/components/pages/home.jsx
+++ frontend/components/pages/home.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = (props) => (
-    <div>
-        <h2> I am significant!</h2>
-    </div>
+    <section className="section page-width">
+        <h1 className="content-title">Hello</h1>
+        <p className="content-body">
+            I am a software developer
+        </p>
+    </section>
 );
 
 export default Home;
