35658f444d9a800e723c99b645907893780330eb
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-26 19:48:47 -0700

    Add external links and icons

--- frontend/components/pages/home.jsx
+++ frontend/components/pages/home.jsx
@@ -11,9 +11,14 @@ const Home = (props) => (
                     23 years young software developer. I like to make computers beep for fun.
                 </p>
                 <p>
-                    PHONE: 561-319-2333
+                    <i className="fa fa-phone fa-fw"></i>&nbsp;&nbsp;561.319.2333
                     <br />
-                    EMAIL: PGilbertSchmitt@gmail.com
+                    <i className="fa fa-envelope fa-fw"></i>&nbsp;&nbsp;PGilbertSchmitt@gmail.com
+                </p>
+                <p>
+                    <a src="/" className="external-link"><i className="fa fa-github fa-fw"></i>&nbsp;&nbsp;Github</a>
+                    <br />
+                    <a src="/" className="external-link"><i className="fa fa-linkedin fa-fw"></i>&nbsp;&nbsp;Linkedin</a>
                 </p>
             </div>
         </div>
