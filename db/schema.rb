6d1af472dd2ce730ac00064054957e66c7d4d274
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-28 10:24:00 -0700

    Add unique slug  to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627193345) do
+ActiveRecord::Schema.define(version: 20170628171241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170627193345) do
     t.string "live_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
   end
 
 end
