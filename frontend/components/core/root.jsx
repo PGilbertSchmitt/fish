8b91933b46ec03e2b004220bc5017baf1df475ff
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-28 09:17:28 -0700

    Provide store

--- frontend/components/core/root.jsx
+++ frontend/components/core/root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './app';
 
 const Root = (props) => (
-    <Router>
-        <App />
-    </Router>
+    <Provider store={props.store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
 );
 
 export default Root;
