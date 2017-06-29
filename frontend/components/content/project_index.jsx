7b67bf450bf7c8a8adda2c7a23780cdbc7fc597f
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-28 19:41:20 -0700

    Create project index components

---
+++ frontend/components/content/project_index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import ProjectIndexItem from './project_index_container.js';
+
+class ProjectIndex extends Component {
+    constructer(props) {
+        super(props);
+    }
+
+    render() {
+        <div className="project-index">
+
+        </div>
+    }
+}
+
+export default ProjectIndex;
