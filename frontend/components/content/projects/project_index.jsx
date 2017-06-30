5ff4a22e26621935549028f3f7e59305578f1076
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-30 13:58:25 -0700

    Get images uploading properly

--- frontend/components/content/projects/project_index.jsx
+++ frontend/components/content/projects/project_index.jsx
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import ProjectIndexItem from './project_index_container.js';
+import { imgSrc } from '../../../util/util.js';
 
 class ProjectIndex extends Component {
     constructor(props) {
         super(props);
+
+        this.renderItems = this.renderItems.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.fetchProjects();
+    }
+
+    renderItems(projects) {
+        // console.log(this.props.projects);
+        return projects.map((project, idx) => (
+            <div>
+                <p>{project.title}</p>
+                <img src={imgSrc(project.img_url)} />
+            </div>
+        ));
     }
 
     render() {
         return (
             <div className="project-index">
-                This is a project
+                {this.renderItems(this.props.projects)}
             </div>
         );
     }
