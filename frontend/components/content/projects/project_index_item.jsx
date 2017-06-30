1f310f3bc5921537f730ee00dfe69e320ba4af3e
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-30 14:48:57 -0700

    Style project index

--- frontend/components/content/projects/project_index_item.jsx
+++ frontend/components/content/projects/project_index_item.jsx
@@ -6,9 +6,8 @@ import { imgSrc } from '../../../util/util';
 const ProjectIndexItem = ({ project: { title, desc, slug, img_url } }) => (
     <Link to={`/porfolio/${slug}`}>
         <div className="project-listing">
-            <div className="index-img-box">
-                <img src={imgSrc(img_url)} className="index-img" />
-            </div>
+            <img src={imgSrc(img_url)} className="index-img" />
+
             <div className="index-text-box">
                 <h5>{title}</h5>
                 <p>{desc}</p>
