91de6501b555c3c823ddabcc3447f856a7ef97c9
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-30 18:36:32 -0700

    Grab article on mount

--- frontend/components/content/projects/project_show.jsx
+++ frontend/components/content/projects/project_show.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ProjectShow = props => (
-    <div>I am not a lemon!</div>
-);
+class ProjectShow extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        let slug = this.props.match.params.slug;
+        this.props.getProject(slug);
+    }
+
+    render() {
+        return (
+            <div>Just a lemon</div>
+        )
+    }
+}
 
 export default ProjectShow;
