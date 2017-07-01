d9e01a329e90d220c510173197117d3b2cc84505
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-06-30 20:21:53 -0700

    Style page and account for incorrect slug

--- frontend/components/content/projects/project_show.jsx
+++ frontend/components/content/projects/project_show.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class ProjectShow extends Component {
     constructor(props) {
         super(props);
+
+        this.replaceHTML = this.replaceHTML.bind(this);
     }
 
     componentDidMount() {
@@ -10,9 +12,22 @@ class ProjectShow extends Component {
         this.props.getProject(slug);
     }
 
+    componentDidUpdate() {
+        this.replaceHTML("project-target");
+    }
+
+    replaceHTML(id) {
+        let text = this.props.project.project;
+
+        document.getElementById(id).innerHTML = Boolean(this.props.project.project)
+            ? this.props.project.project
+            : `<div class="no-project">Could not find a project at portfolio/${this.props.match.params.slug}</div>`
+    }
+
     render() {
+
         return (
-            <div>Just a lemon</div>
+            <div id="project-target" className="project-target"></div>
         )
     }
 }
