8873ed60b8390c3fd655d8baec4ca64157a98cb6
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-04 22:32:59 -0700

    Create github stats component

---
+++ frontend/components/content/github_statistics.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class GithubStatistics extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log("Call for data here");
+    }
+
+    render() {
+        return (
+            <div>I typed today!</div>
+        );
+    }
+}
