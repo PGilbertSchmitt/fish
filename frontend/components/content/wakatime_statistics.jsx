321761beea18ffac9973c7ece738f9e046aa1a0c
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-04 22:33:36 -0700

    Create wakatime stats component

---
+++ frontend/components/content/wakatime_statistics.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class WakatimeStatistics extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        console.log("Call for data here");
+    }
+
+    render() {
+        return (
+            <div>I typed today!</div>
+        );
+    }
+}
