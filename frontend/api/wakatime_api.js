6bcc96092b189069c5680e4a29c1d052ad7beef6
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-05 21:35:58 -0700

    Get over it and use jquery ajax (it just works, okay?)

--- frontend/api/wakatime_api.js
+++ frontend/api/wakatime_api.js
@@ -1,4 +1,15 @@
 export const fetchWakaStats = () => (
-    fetch("https://wakatime.com/share/@pgilbertschmitt/e6fb3b68-184b-4ee8-a714-9bfa2b4f954d.json")
-        .then(res => (res.json()))
+    // fetch(
+    //     "https://wakatime.com/share/@pgilbertschmitt/e6fb3b68-184b-4ee8-a714-9bfa2b4f954d.json",
+    //     { mode: 'cors' }
+    // ).then(res => (res.json()))
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://wakatime.com/share/@pgilbertschmitt/10b44441-8eec-4163-a543-81708d9f13d5.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            console.log(response.data);
+        }
+    })
 );
