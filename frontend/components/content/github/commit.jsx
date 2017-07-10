21b5488abc0e847fa50bc3a6d45292117c4a05e0
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-10 13:28:58 -0700

    Use date to show relative time

--- frontend/components/content/github/commit.jsx
+++ frontend/components/content/github/commit.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import moment from 'moment';
+
+const sinceToday = date => (
+    moment(date).fromNow()
+);
 
 const Commit = ({ commit: { message, merge, date, sha, url } }) => {
     let cssClasses = "gh-icon ";
@@ -9,7 +14,7 @@ const Commit = ({ commit: { message, merge, date, sha, url } }) => {
             <i className={cssClasses} />
             <div className="gh-info">
                 <p className="gh-message">{message}</p>
-                <p className="gh-message">Commit <a className="external-link" href={url}>{sha}</a> made 3 hours ago</p>
+                <p className="gh-message">Commit <a className="external-link" href={url}>{sha}</a> made {moment(date).fromNow()}</p>
             </div>
         </div>
     );
