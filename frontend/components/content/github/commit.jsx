70bb26eef8977ed915e3a1c0660144ccdf6e95f1
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-10 13:12:40 -0700

    Organize commit messages

--- frontend/components/content/github/commit.jsx
+++ frontend/components/content/github/commit.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Commit = ({ commit: { message, merge, date } }) => {
+const Commit = ({ commit: { message, merge, date, sha } }) => {
     let cssClasses = "gh-icon ";
     cssClasses += merge ? "gh-merge" : "gh-commit";
 
     return (
         <div className="gh-item">
             <i className={cssClasses} />
-            <span className="gh-message">{message}</span>
+            <div className="gh-info">
+                <p className="gh-message">{message}</p>
+                <p className="gh-message">Commit {sha} made 3 hours ago</p>
+            </div>
         </div>
     );
 };
