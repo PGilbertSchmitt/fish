465c559b02957332c178af0a5ad44c54de9a11f2
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-11 14:40:49 -0700

    Add temporary work in progress page for blog

---
+++ frontend/components/content/blog/blog_index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BlogIndex = props => (
+    <div className="blog-index">
+        <h1>Work in progress</h1>
+        <h3>Come back later</h3>
+    </div>
+);
+
+export default BlogIndex;
--- frontend/components/core/main.jsx
+++ frontend/components/core/main.jsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/home';
 import Portfolio from '../pages/portfolio';
+import Blog from '../content/blog/blog_index';
 
 const Main = (props) => (
     <main>
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/portfolio" component={Portfolio} />
+            <Route path="/blog" component={Blog} />
         </Switch>
     </main>
 );
