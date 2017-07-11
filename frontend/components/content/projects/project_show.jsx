81eb47c278be50d670e1722115c8053bb371d730
Gilbert Schmitt <pgilbertschmitt@gmail.com>
2017-07-11 11:12:41 -0700

    Style project header

--- frontend/components/content/projects/project_show.jsx
+++ frontend/components/content/projects/project_show.jsx
@@ -18,12 +18,15 @@ class ProjectShow extends Component {
         if (project) {
             return (
                 <div className="project-page">
-                    <h1>{project.title}</h1>
-                    <p>
-                        <a href={`https://github.com/${project.github_url}`} className="external-link"><i className="fa fa-github-square fa-fw"></i>&nbsp;&nbsp;Github repo</a>
-                        <br />
-                        <a href={project.live_url} className="external-link"><i className="fa fa-external-link-square fa-fw"></i>&nbsp;&nbsp;Live page</a>
-                    </p>
+                    <div className="title-bar">
+                        <h1>{project.title}</h1>
+                        <p>
+                            <a href={`https://github.com/${project.github_url}`} className="external-link"><i className="fa fa-github-square fa-fw"></i>&nbsp;&nbsp;Github repo</a>
+                            <br />
+                            <a href={project.live_url} className="external-link"><i className="fa fa-external-link-square fa-fw"></i>&nbsp;&nbsp;Live page</a>
+                        </p>
+                    </div>
+                    <hr />
                     <img className="header-img" src={imgSrc(project.header_img_path)} />
                     <div className="project-target" dangerouslySetInnerHTML={{ __html: project.html }}></div>
                 </div>
